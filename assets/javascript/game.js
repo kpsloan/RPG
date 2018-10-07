7845b8fd7aa7f78d0814fe65b9ec962f90106b76
Jest <kyle.p.sloan@gmail.com>
2018-10-07 18:51:50 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
-// Global variables
+
 var baseAttack = 0;
 var player;
 var defender;
 var charArray = [];
-var playerChosen = false;
-var defenderChosen = false;
+var playerSelected = false;
+var defenderSelected = false;
 
-//Constructor - builds character
+
+$(window).on("load", function() {
+    console.log(document.getElementById("my_audio"));
+});
+
 function Character(name, hp, ap, cp, pic) {
     this.name = name;
     this.healthPoints = hp;
@@ -16,14 +20,11 @@ function Character(name, hp, ap, cp, pic) {
 }
 
 
-
-// Increase the attack strength (this attack strength + original attack strength)
 Character.prototype.increaseAttack = function () {
     this.attackPower += baseAttack;
 };
 
 
-// Performs an attack
 Character.prototype.attack = function (Obj) {
     Obj.healthPoints -= this.attackPower;
     $("#msg").html("You attacked " +
@@ -32,31 +33,26 @@ Character.prototype.attack = function (Obj) {
 };
 
 
-// Performs a counter attack
 Character.prototype.counterAttack = function (Obj) {
     Obj.healthPoints -= this.counterPower;
     $("#msg").append("<br>" + this.name + " countered " + this.counterPower + " damage");
 };
 
 
-// Initialize all the characters
 function initCharacters() {
-    var han = new Character("Han Solo", 120, 10, 5, "./assets/images/han.jpg");
-    var yoda = new Character("Yoda", 200, 50, 30, "./assets/images/yoda.jpg");
-    var jango = new Character("Jango Fett", 150, 15, 2, "./assets/images/jango.jpg");
-    var vader = new Character("Darth Vader", 180, 30, 12, "./assets/images/vader.jpg");
+    var han = new Character("Han Solo", 320, 17, 25, "./assets/images/han.jpg");
+    var yoda = new Character("Yoda", 290, 20, 35, "./assets/images/yoda.jpg");
+    var jango = new Character("Jango Fett", 325, 18, 25, "./assets/images/jango.jpg");
+    var vader = new Character("Darth Vader", 335, 19, 30, "./assets/images/vader.jpg");
     charArray.push(han, yoda, jango, vader);
 }
 
 
-// "Save" the original attack value
 function setBaseAttack(Obj) {
     baseAttack = Obj.attackPower;
 }
 
 
-
-// Checks if character is alive
 function isAlive(Obj) {
     if (Obj.healthPoints > 0) {
         return true;
@@ -65,7 +61,6 @@ function isAlive(Obj) {
 }
 
 
-// Checks if the player has won
 function isWinner() {
     if (charArray.length == 0 && player.healthPoints > 0)
         return true;
@@ -73,7 +68,6 @@ function isWinner() {
 }
 
 
-// Create the character cards onscreen
 function characterCards(divID) {
     $(divID).children().remove();
     for (var i = 0; i < charArray.length; i++) {
@@ -88,13 +82,12 @@ function characterCards(divID) {
         $(divID + " img:last-child").addClass("img-thumbnail");
         $(divID + " div:last-child").append(charArray[i].name + "<br>");
         $(divID + " div:last-child").append("HP: " + charArray[i].healthPoints);
-        $(divID + " idv:last-child").append();
+        $(divID + " div:last-child").append();
 
     }
 }
 
 
-// Update the characters pictures location on the screen (move them between divs)
 function updatePics(fromDivID, toDivID) {
     $(fromDivID).children().remove();
     for (var i = 0; i < charArray.length; i++) {
@@ -102,52 +95,48 @@ function updatePics(fromDivID, toDivID) {
         $(toDivID + " img:last-child").attr("id", charArray[i].name);
         $(toDivID + " img:last-child").attr("src", charArray[i].pic);
         $(toDivID + " img:last-child").attr("width", 150);
-        $(divID + " img:last-child").attr("height", 200);
+        $(toDivID + " img:last-child").attr("height", 200);
         $(toDivID + " img:last-child").addClass("img-thumbnail");
     }
 }
 
 
-// plays audio file (.mp3)
 function playAudio() {
-    var audio = new Audio(".assets/media/cantina-song.mp3");
-    audio.play();
+    var audio2 = new Audio("assets/media/cantina-song.mp3");
+    audio2.play();
 }
 
 
-// Change the view from the first screen to the second screen
 function changeView() {
     $("#startScreen").empty();
     $("#fightScreen").show();
 }
 
 
-// Stores the defender the user has clicked on in the defender variable and removes it from the charArray
 $(document).on("click", "img", function () {
     if (playerSelected && !defenderSelected && (this.id != player.name)) {
         for (var j = 0; j < charArray.length; j++) {
             if (charArray[j].name == (this).id) {
-                defender = charArray[j]; // sets defender
+                defender = charArray[j]; 
                 charArray.splice(j, 1);
                 defenderSelected = true;
                 $("#msg").html("Click to attack!");
             }
         }
-        $("#defender").append(this); // appends the selected defender to the div 
+        $("#defender").append(this); 
         $("#defender").append("<br>" + defender.name);
         $("#defenderHealth").append("HP: " + defender.healthPoints);
         
     }
 
-// Stores the character the user has clicked on in the player variable and removes it from charArray
 if (!playerSelected) {
     for (var i = 0; i < charArray.length; i++) {
         if (charArray[i].name == (this).id) {
-            player = charArray[i]; // sets current player
+            player = charArray[i]; 
             playAudio(); // starts theme song
             $("body").css({
                 "background-image": "url('./assets/images/" + this.id[0] + ".jpg')"
-            }); // changes the background picture according to the user selection
+            }); 
             setBaseAttack(player);
             charArray.splice(i, 1);
             playerSelected = true;
@@ -156,7 +145,7 @@ if (!playerSelected) {
         }
     }
     updatePics("#game", "#defendersLeft");
-    $("#player").append(this); // appends the selected player to the div
+    $("#player").append(this); 
     $("#player").append(player.name);
     $("#playerHealth").append("HP: " + player.healthPoints);
     
@@ -165,7 +154,6 @@ if (!playerSelected) {
 });
 
 
-// The attack button functionality
 $(document).on("click", "#attackBtn", function () {
     if (playerSelected && defenderSelected) {
         if (isAlive(player) && isAlive(defender)) {
@@ -182,14 +170,12 @@ $(document).on("click", "#attackBtn", function () {
                 $("#playerHealth").html("YOU LOSE!");
                 $("#msg").html("Try again?");
                 $("#attackBtn").html("Restart Game");
-                $(document).on("click", "#attackBtn", function () { // restarts game
+                $(document).on("click", "#attackBtn", function () { 
                     location.reload();
                 });
             }
         }
         if (!isAlive(defender)) {
-            $("#defender").removeClass("animated zoomInRight");
-            $("#defenderHealth").removeClass("animated zoomInRight");
             $("#defender").children().remove();
             $("#defender").html("");
             $("#defenderHealth").html("");
@@ -197,14 +183,16 @@ $(document).on("click", "#attackBtn", function () {
             if (isWinner()) {
                 $("#fightScreen").hide();
                 $("#winnerMsg").show();
+                $("#attackBtn").html("Play Again?");
+                $(document).on("click", "#attackBtn", function () { 
+                    location.reload();
+                });    
             }
         }
     }
 });
 
 
-
-// EXECUTE
 $(document).ready(function () {
     $("#fightScreen").hide();
     $("#winnerMsg").hide();
